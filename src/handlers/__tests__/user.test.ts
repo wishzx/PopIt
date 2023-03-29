fd38500131d2db25e2bc7c6c45580cf27ea61dbb
wishzx <gabriele.giganti@gmail.com>
2023-03-29 22:37:45 +0200

    added unit test

--- src/handlers/__tests__/user.test.ts
+++ src/handlers/__tests__/user.test.ts
@@ -1,5 +1,15 @@
+import * as user from '../user';
+
+// use a local database for testing
+// delete db before every test , every test must be stateless and not reliant to another test
 describe("user handler", () => {
-    it("should do", () => {
-        expect(1).toBe(1);
+    it("should create a new user",async () => {
+        const req = {body: {username: 'hello', password: 'hi'}}
+        const res = {json({token}) {
+            expect(token).toBeTruthy() //spy
+        }}
+        
+        const newUser = await user.createNewUser(req,res,()=>{})
+
     });
 });
